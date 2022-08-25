a0bf033eb2c9fbc3bfc76d204660e43f41b2b7ea
JuanCarlos31 <62727772+JuanCarlos31@users.noreply.github.com>
2022-08-25 13:25:41 -0500

    Update Javascript.js
    Se actualizan las URL

--- Javascript.js
+++ Javascript.js
@@ -5,16 +5,16 @@ function hola(){
 function link(tienda){
     switch (tienda) {
       case "clickauto":
-        window.open("https://clikauto.com/");
+        window.open("https://clikauto.com/autos-seminuevos");
       break;
       case "olx":
-        window.open('https://www.olxautos.com.mx/');
+        window.open('https://www.olxautos.com.mx/comprar-un-auto');
       break;
       case "habolt":
-        window.open('https://habolt.mx/');
+        window.open('https://habolt.mx/marketplace?page=1');
       break;
       case "odetta":
-        window.open('https://odetta.com/');
+        window.open('https://odetta.com/autos-usados');
       break;
     }
   }
